7fdc401a81d06ecdb92843a494aab8c19c9992f5
Shashank Sahay <sahayshashank28@gmail.com>
2026-02-22 16:54:03 +0000

    Deployment to gh-pages using github actions

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -4,22 +4,27 @@ import mdx from "@mdx-js/rollup";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    mdx(
-      {
-        providerImportSource: "@mdx-js/react"
-      }
-    ),
-    tailwindcss()
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  return ({
+    plugins: [
+      react(),
+      mdx(
+        {
+          providerImportSource: "@mdx-js/react"
+        }
+      ),
+      tailwindcss()
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  base: process.env.NODE_ENV === "production"
-    ? "/"
-    : "/",
+    base: mode === "production"
+      ? "/"
+      : "/",
+    build: {
+      outDir: '../build'
+    }
+  })
 });
